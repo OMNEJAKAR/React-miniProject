913a64b010b75196ba92390f8abed2bb07979696
Omkar-Arundekar <158194607+Omkar-Arundekar@users.noreply.github.com>
2024-11-18 10:16:18 +0530

    Update Header.jsx

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import Login from "../Login";
-import Button from "../buttons";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./Header.css";
 
 function Header()
 {
   return (
     <div className="navbar">
       <div className="title">
-        <h1>Seva</h1>
+        <img className="log" src="SEVA2.png" height="100px" width="100px"/>
+        <p>Seva</p>
       </div>
         <div className="buttons">
-           <Button name="Home" path="https://www.google.com/" />
-           <Button name="contact" path="https://www.google.com/" /> 
-           <Button name="Wallet" path="https://www.google.com/"/>
+        <nav>
+          <Link  to="/" className="border-hover">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/aboutus" className="border-hover">About</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link  to="/" className="border-hover">Events</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/aboutus" className="border-hover">Profile</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/aboutus" className="border-hover">Contact</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/aboutus" className="border-hover">Wallet</Link>
+          
+        </nav>
+
         </div>
-        <Login />
-    </div>
-  
+        <span className="Login"><Link to="/login" ><h1>Login</h1></Link></span>
+        {/* <Login /> */}
+    </div>  
+
 );
 }
 
